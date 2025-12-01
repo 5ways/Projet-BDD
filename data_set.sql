54738b78f6b2805ffcb5f0b4c9d3e3db4698a060
5ways <163316244+5ways@users.noreply.github.com>
2025-12-01 22:49:41 +0100

    ajout chevaux trot et entraineur

--- data_set.sql
+++ data_set.sql
@@ -1,91 +1,219 @@
--- Jockey de galop 
-INSERT INTO Jockey (jockeyid, nom, prenom, nationalite, poids, discipline, gain)
-VALUES (1, 'Barzalona', 'Mickaël', 'Francais', 56, 'Galop', 0),
-       (2, 'Guyon', 'Maxime', 'Francais', 57, 'Galop', 0),
-       (3, 'Pouchin', 'Alexis', 'Francais', 58, 'Galop', 0),
-       (4, 'Soumillon', 'Christophe', 'Belge', 55, 'Galop', 0),
-       (5, 'Lefebvre', 'Clément', 'Francais', 53, 'Galop', 0),
-       (6, 'Bachelot', 'Théo', 'Francais', 52, 'Galop', 0),
-       (7, 'Pasquier', 'Stéphane', 'Francais', 59.5, 'Galop', 0),
-       (8, 'Vélon', 'Marie', 'Francais', 51, 'Galop', 0),
-       (9, 'Piccone', 'Tony', 'Francais', 55, 'Galop', 0),
-       (10, 'Wayne', 'Jordan', 'Irlandais', 60.5, 'Galop', 0),
-       (11, 'Keane', 'Colin', 'Irlandais', 58, 'Galop', 0),
-       (12, 'Atzeni', 'Andrea', 'Italien', 60.5, 'Galop', 0),
-       (13, 'McMonagle', 'Dylan', 'Irlandais', 59, 'Galop', 0),
-       (14, 'Mendizabal', 'Ioritz', 'Espagnol', 58, 'Galop', 0),
-       (15, 'Kitamura', 'Yuichi', 'Japonais', 57, 'Galop', 0),
-       (16, 'Murphy', 'Oisin', 'Irlandais', 56.5, 'Galop', 0),
-       (17, 'Demuro', 'Cristian', 'Italien', 54, 'Galop', 0),
-       (18, 'Buick', 'William', 'Anglais', 61.5, 'Galop', 0),
-       (19, 'Foley', 'Shane', 'Australien', 57, 'Galop', 0),
-       (20, 'Lemaire', 'Christophe', 'Francais', 56, 'Galop', 0),
-       (21, 'Marquand', 'Tom', 'Anglais', 60, 'Galop', 0),
-       (22, 'Doyle', 'Hollie', 'Anglai', 51.5, 'Galop', 0),
-       (23, 'Orani', 'Antonio', 'Francais', 54, 'Galop', 0),
-       (24, 'Madamet', 'Augustin', 'Francais', 53, 'Galop', 0),
-       (25, 'Michel', 'Mickaëlle', 'Francais', 50, 'Galop', 0),
-       (26, 'Thomas', 'Ronan', 'Francais', 58, 'Galop', 0),
-       (27, 'Journiac', 'Hugo', 'Francais', 55, 'Galop', 0),
-       (28, 'Roussel', 'Alexandre', 'Francais', 56, 'Galop', 0),
-       (29, 'Molins', 'Ambre', 'Francais', 49, 'Galop', 0),
-       (30, 'Pacaut', 'Coralie', 'Francais', 50, 'Galop', 0),
-       (31, 'Eon', 'Marie', 'Francais', 51, 'Galop', 0),
-       (32, 'Chevillard', 'Théo', 'Francais', 54, 'Galop', 0),
-       (33, 'Tosaki', 'Kaito', 'Japonais', 55, 'Galop', 0),
-       (34, 'Moreira', 'Joao', 'Brésilien', 52, 'Galop', 0),
-       (35, 'Crastus', 'Anthony', 'Francais', 56, 'Galop', 0),
-       (36, 'Martin', 'Sophie', 'Francais', 52, 'Galop', 0),
-       (37, 'Lane', 'Damien', 'Australien', 59, 'Galop', 0),
-       (38, 'Lopez', 'Carlos', 'Espagnol', 57, 'Galop', 0),
-       (39, 'Gonzalez', 'Santiago', 'Espagnol', 60, 'Galop', 0),
-       (40, 'Moore', 'Ryan', 'Anglais', 59.5, 'Galop', 0);
-
--- Jockey de trot
 INSERT INTO Jockey (jockeyid, nom, prenom, nationalite, poids, discipline, gain)
 VALUES
-(41, 'Raffin', 'Eric', 'Francais', 62, 'Trot', 0),
-(42, 'Bazire', 'Jean-Michel', 'Francais', 66, 'Trot', 0),
-(43, 'Abrivard', 'Matthieu', 'Francais', 61, 'Trot', 0),
-(44, 'Abrivard', 'Alexandre', 'Francais', 59, 'Trot', 0),
-(45, 'Thomain', 'David', 'Francais', 63, 'Trot', 0),
-(46, 'Rochard', 'Benjamin', 'Francais', 62, 'Trot', 0),
-(47, 'Lebourgeois', 'Yoann', 'Francais', 61, 'Trot', 0),
-(48, 'Nivard', 'Franck', 'Francais', 62, 'Trot', 0),
-(49, 'Barrier', 'Anthony', 'Francais', 64, 'Trot', 0),
-(50, 'Gelormini', 'Gabriele', 'Italien', 60, 'Trot', 0),
-(51, 'Monclin', 'Jean-Philippe', 'Francais', 61, 'Trot', 0),
-(52, 'LeBeller', 'Tony', 'Francais', 63, 'Trot', 0),
-(53, 'Derieux', 'Romain', 'Francais', 62, 'Trot', 0),
-(54, 'Vercruysse', 'Pierre', 'Francais', 64, 'Trot', 0),
-(55, 'Ensch', 'Nicolas', 'Francais', 65, 'Trot', 0),
-(56, 'Guelpa', 'Junior', 'Francais', 62, 'Trot', 0),
-(57, 'Daugeard', 'Philippe', 'Francais', 63, 'Trot', 0),
-(58, 'Ouvrie', 'Franck', 'Francais', 61, 'Trot', 0),
-(59, 'Mottier', 'Mathieu', 'Francais', 59, 'Trot', 0),
-(60, 'Ploquin', 'Paul-Philippe', 'Francais', 58, 'Trot', 0),
-(61, 'Duvaldestin', 'Théo', 'Francais', 57, 'Trot', 0),
-(62, 'Duvaldestin', 'Clément', 'Francais', 56, 'Trot', 0),
-(63, 'Bekaert', 'David', 'Francais', 62, 'Trot', 0),
-(64, 'Roubaud', 'Jean-Marie', 'Francais', 64, 'Trot', 0),
-(65, 'Verva', 'Pierre-Yves', 'Francais', 63, 'Trot', 0),
-(66, 'Callier', 'Pierre', 'Francais', 64, 'Trot', 0),
-(67, 'Lamy', 'Etienne', 'Francais', 59, 'Trot', 0),
-(68, 'Souloy', 'Fabrice', 'Francais', 65, 'Trot', 0),
-(69, 'Bigeon', 'Christian', 'Francais', 63, 'Trot', 0),
-(70, 'Wiels', 'Antoine', 'Francais', 62, 'Trot', 0),
-(71, 'LeBeller', 'Bertrand', 'Francais', 61, 'Trot', 0),
-(72, 'Monthule', 'Hugues', 'Francais', 61, 'Trot', 0),
-(73, 'Martin', 'Guillaume', 'Francais', 59, 'Trot', 0),
-(74, 'Gocciadoro', 'Alessandro', 'Italien', 62, 'Trot', 0),
-(75, 'Goop', 'Bjorn', 'Suedois', 61, 'Trot', 0),
-(76, 'Verbeeck', 'Jos', 'Belge', 64, 'Trot', 0),
-(77, 'Tiepolo', 'Luca', 'Italien', 63, 'Trot', 0),
-(78, 'Berg', 'Erik', 'Suedois', 62, 'Trot', 0),
-(79, 'Silva', 'Carlos', 'Portugais', 60, 'Trot', 0),
-(80, 'Johansson', 'Oskar', 'Suedois', 61, 'Trot', 0);
+-- Jockeys de Galop
+(1, 'Barzalona', 'Mickaël', 'Francais', 56.3, 'Galop', 0),
+(2, 'Guyon', 'Maxime', 'Francais', 57.3, 'Galop', 0),
+(3, 'Pouchin', 'Alexis', 'Francais', 58.6, 'Galop', 0),
+(4, 'Soumillon', 'Christophe', 'Belge', 55.5, 'Galop', 0),
+(5, 'Lefebvre', 'Clément', 'Francais', 53.8, 'Galop', 0),
+(6, 'Bachelot', 'Théo', 'Francais', 52.4, 'Galop', 0),
+(7, 'Pasquier', 'Stéphane', 'Francais', 59.6, 'Galop', 0),
+(8, 'Vélon', 'Marie', 'Francais', 51.5, 'Galop', 0),
+(9, 'Piccone', 'Tony', 'Francais', 55.3, 'Galop', 0),
+(10, 'Wayne', 'Jordan', 'Irlandais', 60.5, 'Galop', 0),
+(11, 'Keane', 'Colin', 'Irlandais', 58.6, 'Galop', 0),
+(12, 'Atzeni', 'Andrea', 'Italien', 60.8, 'Galop', 0),
+(13, 'McMonagle', 'Dylan', 'Irlandais', 59.2, 'Galop', 0),
+(14, 'Mendizabal', 'Ioritz', 'Espagnol', 58.5, 'Galop', 0),
+(15, 'Kitamura', 'Yuichi', 'Japonais', 57.3, 'Galop', 0),
+(16, 'Murphy', 'Oisin', 'Irlandais', 56.1, 'Galop', 0),
+(17, 'Demuro', 'Cristian', 'Italien', 54.5, 'Galop', 0),
+(18, 'Buick', 'William', 'Anglais', 61.8, 'Galop', 0),
+(19, 'Foley', 'Shane', 'Australien', 57.5, 'Galop', 0),
+(20, 'Lemaire', 'Christophe', 'Francais', 56.2, 'Galop', 0),
+(21, 'Marquand', 'Tom', 'Anglais', 60.8, 'Galop', 0),
+(22, 'Doyle', 'Hollie', 'Anglais', 51.1, 'Galop', 0),
+(23, 'Orani', 'Antonio', 'Francais', 54.1, 'Galop', 0),
+(24, 'Madamet', 'Augustin', 'Francais', 53.9, 'Galop', 0),
+(25, 'Michel', 'Mickaëlle', 'Francais', 50.9, 'Galop', 0),
+(26, 'Thomas', 'Ronan', 'Francais', 58.2, 'Galop', 0),
+(27, 'Journiac', 'Hugo', 'Francais', 55.9, 'Galop', 0),
+(28, 'Roussel', 'Alexandre', 'Francais', 56.1, 'Galop', 0),
+(29, 'Molins', 'Ambre', 'Francais', 49.4, 'Galop', 0),
+(30, 'Pacaut', 'Coralie', 'Francais', 50.2, 'Galop', 0),
+(31, 'Eon', 'Marie', 'Francais', 51.5, 'Galop', 0),
+(32, 'Chevillard', 'Théo', 'Francais', 54.5, 'Galop', 0),
+(33, 'Tosaki', 'Kaito', 'Japonais', 55.8, 'Galop', 0),
+(34, 'Moreira', 'Joao', 'Brésilien', 52.2, 'Galop', 0),
+(35, 'Crastus', 'Anthony', 'Francais', 56.7, 'Galop', 0),
+(36, 'Martin', 'Sophie', 'Francais', 52.9, 'Galop', 0),
+(37, 'Lane', 'Damien', 'Australien', 59.6, 'Galop', 0),
+(38, 'Lopez', 'Carlos', 'Espagnol', 57.1, 'Galop', 0),
+(39, 'Gonzalez', 'Santiago', 'Espagnol', 60.3, 'Galop', 0),
+(40, 'Moore', 'Ryan', 'Anglais', 59.2, 'Galop', 0),
+
+-- Drivers de Trot 
+(41, 'Raffin', 'Eric', 'Francais', 62.3, 'Trot', 0),
+(42, 'Bazire', 'Jean-Michel', 'Francais', 66.4, 'Trot', 0),
+(43, 'Abrivard', 'Matthieu', 'Francais', 61.1, 'Trot', 0),
+(44, 'Abrivard', 'Alexandre', 'Francais', 59.4, 'Trot', 0),
+(45, 'Thomain', 'David', 'Francais', 63.3, 'Trot', 0),
+(46, 'Rochard', 'Benjamin', 'Francais', 62.7, 'Trot', 0),
+(47, 'Lebourgeois', 'Yoann', 'Francais', 61.6, 'Trot', 0),
+(48, 'Nivard', 'Franck', 'Francais', 62.5, 'Trot', 0),
+(49, 'Barrier', 'Anthony', 'Francais', 64.3, 'Trot', 0),
+(50, 'Gelormini', 'Gabriele', 'Italien', 60.2, 'Trot', 0),
+(51, 'Monclin', 'Jean-Philippe', 'Francais', 61.3, 'Trot', 0),
+(52, 'LeBeller', 'Tony', 'Francais', 63.4, 'Trot', 0),
+(53, 'Derieux', 'Romain', 'Francais', 62.7, 'Trot', 0),
+(54, 'Vercruysse', 'Pierre', 'Francais', 64.7, 'Trot', 0),
+(55, 'Ensch', 'Nicolas', 'Francais', 65.7, 'Trot', 0),
+(56, 'Guelpa', 'Junior', 'Francais', 62.6, 'Trot', 0),
+(57, 'Daugeard', 'Philippe', 'Francais', 63.7, 'Trot', 0),
+(58, 'Ouvrie', 'Franck', 'Francais', 61.1, 'Trot', 0),
+(59, 'Mottier', 'Mathieu', 'Francais', 59.3, 'Trot', 0),
+(60, 'Ploquin', 'Paul-Philippe', 'Francais', 58.5, 'Trot', 0),
+(61, 'Duvaldestin', 'Théo', 'Francais', 57.9, 'Trot', 0),
+(62, 'Duvaldestin', 'Clément', 'Francais', 56.4, 'Trot', 0),
+(63, 'Bekaert', 'David', 'Francais', 62.3, 'Trot', 0),
+(64, 'Roubaud', 'Jean-Marie', 'Francais', 64.1, 'Trot', 0),
+(65, 'Verva', 'Pierre-Yves', 'Francais', 63.5, 'Trot', 0),
+(66, 'Callier', 'Pierre', 'Francais', 64.6, 'Trot', 0),
+(67, 'Lamy', 'Etienne', 'Francais', 59.9, 'Trot', 0),
+(68, 'Souloy', 'Fabrice', 'Francais', 65.3, 'Trot', 0),
+(69, 'Bigeon', 'Christian', 'Francais', 63.2, 'Trot', 0),
+(70, 'Wiels', 'Antoine', 'Francais', 62.8, 'Trot', 0),
+(71, 'LeBeller', 'Bertrand', 'Francais', 61.6, 'Trot', 0),
+(72, 'Monthule', 'Hugues', 'Francais', 61.4, 'Trot', 0),
+(73, 'Martin', 'Guillaume', 'Francais', 59.3, 'Trot', 0),
+(74, 'Gocciadoro', 'Alessandro', 'Italien', 62.4, 'Trot', 0),
+(75, 'Goop', 'Bjorn', 'Suedois', 61.6, 'Trot', 0),
+(76, 'Verbeeck', 'Jos', 'Belge', 64.2, 'Trot', 0),
+(77, 'Tiepolo', 'Luca', 'Italien', 63.6, 'Trot', 0),
+(78, 'Berg', 'Erik', 'Suedois', 62.9, 'Trot', 0),
+(79, 'Silva', 'Carlos', 'Portugais', 60.8, 'Trot', 0),
+(80, 'Johansson', 'Oskar', 'Suedois', 61.9, 'Trot', 0);
 
 
--- Cheval de galop
+-- Chevaux galop
 INSERT INTO Cheval (chevalid, nom, datenaiss, race, age, sexe, poids, discipline, gain)
-VALUES
+VALUES (1, 'Giavellotto', '2019-04-10', 'Pur-sang', 6, 'M', 565.4, 'Galop', 0),
+(2, 'White Birch', '2020-03-27', 'Pur-sang', 5, 'M', 540.2, 'Galop', 0),
+(3, 'Arrow Eagle', '2021-02-11', 'Pur-sang', 4, 'M', 568.7, 'Galop', 0),
+(4, 'Sosie', '2021-05-02', 'Pur-sang', 4, 'M', 553.1, 'Galop', 0),
+(5, 'Los Angeles', '2021-07-02', 'Pur-sang', 4, 'M', 578.5, 'Galop', 0),
+(6, 'Bizantine Dream', '2021-11-15', 'Pur-sang', 4, 'M', 548.3, 'Galop', 0),
+(7, 'Estrange', '2021-09-05', 'Pur-sang', 4, 'F', 554.6, 'Galop', 0),
+(8, 'Quisisana', '2020-03-19', 'Pur-sang', 5, 'F', 592.2, 'Galop', 0),
+(9, 'Kalpana', '2021-05-30', 'Pur-sang', 4, 'F', 520.8, 'Galop', 0),
+(10, 'Aventure', '2021-04-14', 'Pur-sang', 4, 'F', 528.4, 'Galop', 0),
+(11, 'Daryz', '2022-05-02', 'Pur-sang', 3, 'M', 542.1, 'Galop', 0),
+(12, 'Leffard', '2021-01-22', 'Pur-sang', 4, 'M', 559.9, 'Galop', 0),
+(13, 'Cualificar', '2020-04-05', 'Pur-sang', 5, 'M', 568.3, 'Galop', 0),
+(14, 'Hotazhell', '2020-02-28', 'Pur-sang', 5, 'M', 573.5, 'Galop', 0),
+(15, 'Croix du Nord', '2021-03-22', 'Pur-sang', 4, 'M', 551.2, 'Galop', 0),
+(16, 'Alohi Alii', '2019-02-10', 'Pur-sang', 6, 'F', 538.7, 'Galop', 0),
+(17, 'Minnie Hauk', '2022-04-15', 'Pur-sang', 3, 'F', 524.3, 'Galop', 0),
+(18, 'Gezora', '2021-04-01', 'Pur-sang', 4, 'F', 533.6, 'Galop', 0),
+(19, 'Il Est Français', '2017-04-24', 'AQPS', 8, 'H', 590.5, 'Galop', 0),
+(20, 'Hello Youmzain', '2016-03-18', 'Pur-sang', 9, 'M', 575.2, 'Galop', 0),
+(21, 'Juntos Ganamos', '2019-05-11', 'Anglo-Arabe', 6, 'H', 515.8, 'Galop', 0),
+(22, 'Télème', '2018-02-20', 'AQPS', 7, 'H', 588.4, 'Galop', 0),
+(23, 'Jelitobum', '2019-03-09', 'AQPS', 6, 'M', 567.1, 'Galop', 0),
+(24, 'Icare Allen', '2018-03-30', 'Pur-sang', 7, 'M', 555.3, 'Galop', 0),
+(25, 'Jippie du Cerisier', '2019-04-14', 'Anglo-Arabe', 6, 'F', 505.6, 'Galop', 0),
+(26, 'Raffles Face', '2017-05-22', 'Selle Français', 8, 'H', 610.2, 'Galop', 0),
+(27, 'Hermès Baie', '2017-02-13', 'Selle Français', 8, 'H', 620.5, 'Galop', 0),
+(28, 'Goliath du Berlais', '2016-04-12', 'Selle Français', 9, 'M', 630.1, 'Galop', 0),
+(29, 'Big Rock', '2020-03-11', 'Pur-sang', 5, 'M', 565.4, 'Galop', 0),
+(30, 'Feed The Flame', '2020-02-02', 'Pur-sang', 5, 'M', 575.7, 'Galop', 0),
+(31, 'Blue Rose Cen', '2020-04-27', 'Pur-sang', 5, 'F', 510.3, 'Galop', 0),
+(32, 'Ace Impact', '2020-04-06', 'Pur-sang', 5, 'M', 570.2, 'Galop', 0),
+(33, 'Horizon Dore', '2020-04-08', 'Pur-sang', 5, 'H', 560.8, 'Galop', 0),
+(34, 'Simca Mille', '2019-04-21', 'Pur-sang', 6, 'M', 580.4, 'Galop', 0),
+(35, 'Astre Royal', '2021-04-19', 'AQPS', 4, 'M', 560.6, 'Galop', 0),
+(36, 'Quintessia', '2020-03-12', 'Pur-sang Arabe', 5, 'F', 470.2, 'Galop', 0),
+(37, 'Emerald Falcon', '2019-05-07', 'Anglo-Arabe', 6, 'M', 510.9, 'Galop', 0),
+(38, 'Tornado Lys', '2022-03-30', 'Pur-sang', 3, 'M', 545.3, 'Galop', 0),
+(39, 'Saphir d’Empire', '2018-04-02', 'Selle Français', 7, 'F', 620.1, 'Galop', 0),
+(40, 'Vol de Minuit', '2021-05-25', 'Pur-sang', 4, 'F', 518.5, 'Galop', 0);
+
+-- Chevaux trot
+INSERT INTO Cheval (chevalid, nom, datenaiss, race, age, sexe, poids, discipline, gain)
+VALUES (41, 'Idao de Tillard', '2018-04-12', 'Trotteur Francais', 6, 'M', 510.0, 'Trot', 0),
+(42, 'Horsy Dream', '2017-03-22', 'Trotteur Francais', 7, 'M', 495.5, 'Trot', 0),
+(43, 'City of Troy', '2021-03-07', 'Pur-sang', 3, 'M', 488.2, 'Galop', 0),
+(44, 'Auguste Rodin', '2020-01-26', 'Pur-sang', 4, 'M', 482.7, 'Galop', 0),
+(45, 'Look de Vega', '2021-02-14', 'Pur-sang', 3, 'M', 490.3, 'Galop', 0),
+(46, 'Sosie', '2021-03-15', 'Pur-sang', 3, 'M', 478.9, 'Galop', 0),
+(47, 'Economics', '2021-04-02', 'Pur-sang', 3, 'M', 505.6, 'Galop', 0),
+(48, 'Bluestocking', '2020-05-10', 'Pur-sang', 4, 'F', 465.2, 'Galop', 0),
+(49, 'Rebel''s Romance', '2018-03-18', 'Pur-sang', 6, 'H', 515.7, 'Galop', 0),
+(50, 'Charyn', '2020-04-12', 'Pur-sang', 4, 'M', 500.1, 'Galop', 0),
+(51, 'Goliath', '2020-03-20', 'Pur-sang', 4, 'H', 510.8, 'Galop', 0),
+(52, 'Calandagan', '2021-02-28', 'Pur-sang', 3, 'H', 485.4, 'Galop', 0),
+(53, 'Mqse de Sevigne', '2019-03-22', 'Pur-sang', 5, 'F', 472.6, 'Galop', 0),
+(54, 'Iresine', '2017-04-01', 'Pur-sang', 7, 'H', 480.3, 'Galop', 0),
+(55, 'Zarakem', '2020-04-10', 'Pur-sang', 4, 'M', 498.9, 'Galop', 0),
+(56, 'Haya Zark', '2019-02-15', 'Pur-sang', 5, 'M', 492.2, 'Galop', 0),
+(57, 'Metropolitan', '2021-05-02', 'Pur-sang', 3, 'M', 475.7, 'Galop', 0),
+(58, 'Sparkling Plenty', '2021-04-10', 'Pur-sang', 3, 'F', 455.3, 'Galop', 0),
+(59, 'Ramatuelle', '2021-02-12', 'Pur-sang', 3, 'F', 448.8, 'Galop', 0),
+(60, 'Beauvatier', '2021-03-05', 'Pur-sang', 3, 'M', 482.1, 'Galop', 0),
+(61, 'Facteur Cheval', '2019-03-18', 'Pur-sang', 5, 'H', 502.4, 'Galop', 0),
+(62, 'Horizon Dore', '2020-05-14', 'Pur-sang', 4, 'H', 488.6, 'Galop', 0),
+(63, 'Feed The Flame', '2020-03-28', 'Pur-sang', 4, 'M', 495.9, 'Galop', 0),
+(64, 'Big Rock', '2020-04-15', 'Pur-sang', 4, 'M', 508.2, 'Galop', 0),
+(65, 'Blue Rose Cen', '2020-02-20', 'Pur-sang', 4, 'F', 462.7, 'Galop', 0),
+(66, 'Unquestionable', '2021-05-05', 'Pur-sang', 3, 'M', 470.3, 'Galop', 0),
+(67, 'Los Angeles', '2021-03-25', 'Pur-sang', 3, 'M', 490.8, 'Galop', 0),
+(68, 'Kyprios', '2018-05-10', 'Pur-sang', 6, 'M', 520.4, 'Galop', 0),
+(69, 'Double Major', '2020-02-15', 'Pur-sang', 4, 'H', 485.1, 'Galop', 0),
+(70, 'Junko', '2019-02-25', 'Pur-sang', 5, 'H', 492.6, 'Galop', 0),
+(71, 'Simca Mille', '2019-03-12', 'Pur-sang', 5, 'M', 480.9, 'Galop', 0),
+(72, 'Zagrey', '2019-04-08', 'Pur-sang', 5, 'M', 494.2, 'Galop', 0),
+(73, 'Al Riffa', '2020-03-10', 'Pur-sang', 4, 'M', 500.7, 'Galop', 0),
+(74, 'Shin Emperor', '2021-04-20', 'Pur-sang', 3, 'M', 476.3, 'Galop', 0),
+(75, 'Place du Carrousel', '2019-03-01', 'Pur-sang', 5, 'F', 468.8, 'Galop', 0),
+(76, 'Nashwa', '2019-05-05', 'Pur-sang', 5, 'F', 472.1, 'Galop', 0),
+(77, 'Inspiral', '2019-03-15', 'Pur-sang', 5, 'F', 475.4, 'Galop', 0),
+(78, 'Luxembourg', '2019-01-20', 'Pur-sang', 5, 'M', 505.9, 'Galop', 0),
+(79, 'Bay Bridge', '2018-03-01', 'Pur-sang', 6, 'M', 510.2, 'Galop', 0),
+(80, 'Fantastic Moon', '2020-02-10', 'Pur-sang', 4, 'M', 489.6, 'Galop', 0);
+
+
+INSERT INTO Entraineur (entraineurid, nom, prenom, nationalite, gain)
+VALUES 
+-- Galop
+(1, 'Fabre', 'André', 'Francais', 0),        -- Entraîne Sosie, Tribalist...
+(2, 'O''Brien', 'Aidan', 'Irlandais', 0),    -- Entraîne Los Angeles, City of Troy
+(3, 'Rouget', 'Jean-Claude', 'Francais', 0), -- Entraîne Ace Impact, Arrow Eagle
+(4, 'Graffard', 'Francis-Henri', 'Francais', 0), -- Entraîne Calandagan, Goliath
+(5, 'Head', 'Christopher', 'Francais', 0),   -- Entraîne Blue Rose Cen, Ramatuelle
+(6, 'Botti', 'Marco', 'Italien', 0),         -- Entraîne Giavellotto
+(7, 'Murphy', 'John-Joseph', 'Irlandais', 0),-- Entraîne White Birch
+(8, 'Reynier', 'Jérôme', 'Francais', 0),     -- Entraîne Facteur Cheval
+(9, 'Cottier', 'Patrice', 'Francais', 0),    -- Entraîne Sparkling Plenty
+(10, 'Barberot', 'Yann', 'Francais', 0),     -- Entraîne Beauvatier
+(11, 'Ferland', 'Christophe', 'Francais', 0),-- Entraîne Aventure
+(12, 'Wattel', 'Stéphane', 'Francais', 0),   -- Entraîne Big Rock (ex-entraîneur)
+(13, 'Appleby', 'Charlie', 'Anglais', 0),    -- Godolphin (Rebel's Romance)
+(14, 'Haggas', 'William', 'Anglais', 0),     -- Entraîne Economics
+(15, 'George', 'Noel', 'Anglais', 0),        -- Entraîne Il Est Français
+(16, 'Chaillé-Chaillé', 'Arnaud', 'Francais', 0), -- Entraîne Télème
+(17, 'Nicolle', 'François', 'Francais', 0),  -- Entraîne Hermès Baie
+(18, 'Macaire', 'Guillaume', 'Francais', 0), -- Entraîne Goliath du Berlais (historique)
+(19, 'Cottin', 'David', 'Francais', 0),      -- Entraîne Juntos Ganamos
+(20, 'Leenders', 'Gabriel', 'Francais', 0),  -- Top entraîneur obstacle
+
+-- trot
+(21, 'Duvaldestin', 'Thierry', 'Francais', 0), -- Entraîne Idao de Tillard
+(22, 'Belloche', 'Pierre', 'Francais', 0),     -- Entraîne Horsy Dream
+(23, 'Bazire', 'Jean-Michel', 'Francais', 0),  -- Entraîne Hooker Berry, Jushua Tree
+(24, 'Dersoir', 'Jean-Luc', 'Francais', 0),    -- Entraîne Hokkaido Jiel
+(25, 'Derieux', 'Romain', 'Francais', 0),      -- Entraîne Go On Boy
+(26, 'Robin', 'Benoît', 'Francais', 0),        -- Entraîne Hussard du Landret
+(27, 'Allaire', 'Philippe', 'Francais', 0),    -- Entraîne Italiano Vero, Koctel du Dain
+(28, 'Guarato', 'Sébastien', 'Francais', 0),   -- Entraîne Krack Time Atout
+(29, 'Souloy', 'Fabrice', 'Francais', 0),      -- Entraîne Ampia Mede SM, Gu d'Héripré
+(30, 'Goop', 'Bjorn', 'Suedois', 0),           -- Entraîne San Moteur, Joviality (parfois)
+(31, 'Gocciadoro', 'Alessandro', 'Italien', 0),-- Entraîne Vivid Wise As, Vernissage Grif
+(32, 'Redén', 'Daniel', 'Suedois', 0),         -- Entraîne Don Fanucci Zet, Hail Mary
+(33, 'Abrivard', 'Laurent-Claude', 'Francais', 0), -- Entraîne Inmarosa, Just A Gigolo
+(34, 'Mottier', 'Mathieu', 'Francais', 0),     -- Spécialiste Monté & Attelé
+(35, 'Bigeon', 'Christian', 'Francais', 0),    -- Entraîne Hip Hop Haufor
+(36, 'Thomain', 'Clément', 'Francais', 0),     -- Famille Thomain
+(37, 'Malmqvist', 'Tomas', 'Suedois', 0),      -- Entraîne Jazzy Perrine
+(38, 'Westerink', 'Richard', 'Hollandais', 0), -- Entraîne Etonnant
+(39, 'Roger', 'Sylvain', 'Francais', 0),       -- Entraîne Délia du Pommereux
+(40, 'Nivard', 'Franck', 'Francais', 0);       -- Entraîne aussi (ex: Kool du Caux jadis)
