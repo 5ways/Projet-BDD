98a1dba780ccd07ff7c15ae982279c11943b4a66
5ways <163316244+5ways@users.noreply.github.com>
2025-12-09 20:21:11 +0100

    mise à jour des tables selon le nouveau schéma

--- tables.sql
+++ tables.sql
@@ -13,7 +13,6 @@ CREATE TABLE Cheval(
     nom varchar(20) NOT NULL,
     datenaiss date NOT NULL,
     race varchar(20),
-    age number(2), /*inutile*/
     sexe varchar(10) NOT NULL,
     poids number(5,2),
     discipline varchar(20) NOT NULL,
@@ -47,7 +46,7 @@ CREATE TABLE Organisateur(
 );
 
 CREATE TABLE Course(
-    courseid number(2) PRIMARY KEY,
+    courseid number(3) PRIMARY KEY,
     nom varchar(50) NOT NULL,
     date_c date NOT NULL,
     discipline varchar(20) NOT NULL,
@@ -57,7 +56,7 @@ CREATE TABLE Course(
     cashprize number(8) NOT NULL,
     lieu varchar(50) NOT NULL,
     organisateurid number(2),
-    CONSTRAINT organisateurid FOREIGN KEY (organisateurid) REFERENCES Organisateur(organisateurid)
+    CONSTRAINT fk_course_organisateur FOREIGN KEY (organisateurid) REFERENCES Organisateur(organisateurid)
 );
 
 
@@ -69,66 +68,52 @@ CREATE TABLE Parieur(
     solde number(8) NOT NULL
 );
 
-CREATE TABLE Paris(
-    parisid number(2) PRIMARY KEY,
-    typeparis varchar(20) NOT NULL,
-    montant number(4)NOT NULL,
-    statut varchar(20) NOT NULL
-);
 
 CREATE TABLE Participation(
     participationid number(3) PRIMARY KEY,
     courseid number(2),
-    resultat varchar(20), /*sous quelle forme ?*/
+    resultat varchar(20), /*ou number 1 */
     statut varchar(20) NOT NULL,
-    CONSTRAINT courseid FOREIGN KEY (courseid) REFERENCES Course(courseid)
+    CONSTRAINT fk_part_course FOREIGN KEY (courseid) REFERENCES Course(courseid)
 );
 
-CREATE TABLE Forme(
-    formeid number(3) PRIMARY KEY,
-    jockeyid number(2),
-    chevalid number(2),
-    CONSTRAINT jockeyid FOREIGN KEY (jockeyid) REFERENCES Jockey(jockeyid),
-    CONSTRAINT chevalid FOREIGN KEY (chevalid) REFERENCES Cheval(chevalid)
-);
 
 CREATE TABLE Duo(
     duoid number(3) PRIMARY KEY, 
-    formeid number(3),
-    entraineurid number(2),
+    jockeyid number(2)PRIMARY KEY,
+    chevalid number(2)PRIMARY KEY,
+    entraineurid number(2)PRIMARY KEY,
     discipline varchar(20),
-    CONSTRAINT unq_duo UNIQUE (formeid, entraineurid), 
-    CONSTRAINT fk_duo_entraineur FOREIGN KEY (entraineurid) REFERENCES Entraineur(entraineurid),
-    CONSTRAINT fk_duo_forme FOREIGN KEY (formeid) REFERENCES Forme(formeid)
+    CONSTRAINT unq_duo UNIQUE (duoid, entraineurid), 
+    CONSTRAINT fk_duo_jockey FOREIGN KEY (jockeyid) REFERENCES Jockey(jockeyid),
+    CONSTRAINT fk_duo_cheval FOREIGN KEY (chevalid) REFERENCES Cheval(chevalid),
+    CONSTRAINT fk_duo_entraineur FOREIGN KEY (entraineurid) REFERENCES Entraineur(entraineurid)
 );
 
-CREATE TABLE Parier(
-    parisid number(2) PRIMARY KEY,
-    parieurid number(2) PRIMARY KEY,
-    CONSTRAINT parisid FOREIGN KEY (parisid) REFERENCES Paris(parisid),
-    CONSTRAINT parieurid FOREIGN KEY (parieurid) REFERENCES Parieur(parieurid)
-);
-
-CREATE TABLE Mise(
-    parisid number(2) PRIMARY KEY,
-    participationid number(3) PRIMARY KEY,
-    CONSTRAINT parisid FOREIGN KEY (parisid) REFERENCES Paris(parisid),
-    CONSTRAINT participationid FOREIGN KEY (participationid) REFERENCES Participation(participationid)
-);
 
 CREATE TABLE Inscription(
-    inscriptionid number(3) PRIMARY KEY,
-    participationid number(3),
-    duoid number(3), 
+    participationid number(3)PRIMARY KEY,
+    duoid number(3)PRIMARY KEY, 
     statut varchar(20) NOT NULL,
-    CONSTRAINT participationid FOREIGN KEY (participationid) REFERENCES Participation(participationid),
-    CONSTRAINT parieurid FOREIGN KEY (parieurid) REFERENCES Parieur(parieurid)
+    CONSTRAINT fk_inscrip_participation FOREIGN KEY (participationid) REFERENCES Participation(participationid),
+    CONSTRAINT fk_inscrip_duo FOREIGN KEY (duoid) REFERENCES Duo(duoid)
 );
 
 CREATE TABLE Appartient(
     proprietaireid number(3) PRIMARY KEY,
     chevalid number(2) PRIMARY KEY,
     part number(3),
-    CONSTRAINT proprietaireid FOREIGN KEY (proprietaireid) REFERENCES Propietaire(proprietaireid),
-    CONSTRAINT chevalid FOREIGN KEY (chevalid) REFERENCES Cheval(chevalid)
+    CONSTRAINT fk_app_proprietaire FOREIGN KEY (proprietaireid) REFERENCES Propietaire(proprietaireid),
+    CONSTRAINT fk_app_cheval FOREIGN KEY (chevalid) REFERENCES Cheval(chevalid)
+);
+
+CREATE TABLE Paris(
+    parisid number(3) PRIMARY KEY,
+    parieurid number(2) PRIMARY KEY,
+    participationid number(3) PRIMARY KEY,
+    typeparis varchar(20) NOT NULL,
+    montant number(4)NOT NULL,
+    statut varchar(20) NOT NULL,
+    CONSTRAINT fk_paris_participation FOREIGN KEY (participationid) REFERENCES Participation(participationid),
+    CONSTRAINT fk_paris_parieur FOREIGN KEY (parieurid) REFERENCES Parieur(parieurid)
 );
