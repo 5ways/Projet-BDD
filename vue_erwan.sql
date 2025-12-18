0746320264a6ede28563fbd9400d7009cc66fa8b
Mowowglii <erwann.razatovo@gmail.com>
2025-12-18 15:16:06 +0100

    2ème vue terminée

--- vue_erwan.sql
+++ vue_erwan.sql
@@ -9,3 +9,11 @@ d.chevalid = ch.chevalid AND
 d.jockeyid = j.jockeyid AND
 i.statut = 'Validé'
 ORDER BY c.date_c;
+
+-- Fiche de Propriété et gains 
+CREATE OR REPLACE VIEW fiche_proprietes AS
+SELECT p.prenom as Prenom_Propriétaire, p.nom as Nom_Propriétaire, c.nom as Nom_Cheval, a.part part_de_propriété
+FROM Proprietaire p, Cheval c, Appartient a
+WHERE a.proprietaireid = p.proprietaireid AND
+a.chevalid = c.chevalid
+ORDER BY Nom_Cheval;
